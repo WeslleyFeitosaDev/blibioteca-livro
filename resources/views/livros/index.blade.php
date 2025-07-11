c875373d984bfd7318fbf0af70cfe814131e458a
Weslley Feitosa Barbosa <weslleybarbosa662@gmail.com>
2025-07-11 15:34:50 -0300

    adicionando elementos na pagina

---
+++ resources/views/livros/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca</title>
+</head>
+<body>
+    <div class="box_livros">
+        <h1>Veja os livros desponíveis</h1>
+        <div class="card_livro">
+            <div class="img">
+                <img src="image_card.jpeg" alt="" srcset="">
+            </div>
+            <div class="detalhes"></div>
+            <div class=""></div>
+        </div>
+    </div>
+</body>
+</html>
