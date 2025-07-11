6756e523933f6ed93e048315bdb9eb06577a9745
Weslley Feitosa Barbosa <weslleybarbosa662@gmail.com>
2025-07-11 15:04:16 -0300

    mudando o nome do banco na migração

--- database/migrations/2025_07_11_173307_create_livro_table.php
+++ database/migrations/2025_07_11_173307_create_livro_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('livro', function (Blueprint $table) {
+        Schema::create('livros', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
             $table->string('ator');
