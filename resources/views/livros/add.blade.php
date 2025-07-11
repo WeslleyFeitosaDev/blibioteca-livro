ff4ef0a8582a3b28170a17f95c3b604f1ef821a9
Weslley Feitosa Barbosa <weslleybarbosa662@gmail.com>
2025-07-11 20:46:33 -0300

    colando os elementos em todas as paginas

---
+++ resources/views/livros/add.blade.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add</title>
+    <link href='https://cdn.boxicons.com/fonts/brands/boxicons-brands.min.css' rel='stylesheet'>
+    <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+        *{
+            font-family: "Poppins", sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body{
+            display: flex;
+            width: 100%;
+            height: 100vh;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .box_add{
+            padding: 10px;
+            width: 80%;
+            height: 500px;
+            background-color: #eeeeeeff;
+            border-radius: 15px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            position: absolute;
+        }
+
+        .box_add h1{
+            
+            font-size: 25px;
+            color: #1d165cff;
+        }
+
+        form div{
+            display: flex;
+            flex-direction: column;
+        }
+
+        .box_add form{
+            height: 90%;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+            width: 100%;
+        }
+
+        form input{
+            border-radius: 5px;
+            width: 520px;
+            height: 36px;
+            align-items: center;
+            border: none;
+            outline: none;
+            padding: 10px;
+            margin-top: 5px;
+            margin-bottom: 5px;
+            color: #0e0b2cff;
+        }
+
+        .box_add label{
+            font-size: 14px;
+            color: #2a2a49ff;
+        }
+
+        .bnts {
+            position: relative;
+            display: flex;
+            width: 520px;
+        }
+
+        .bnts button{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 5px;
+            width: 90px;
+            height: 40px;
+            outline: none;
+            border: none;
+            cursor: pointer;
+            transition: all .20s linear;
+        }
+
+        .bnts button:first-child{
+            background-color: #1d165cff;
+            color: #94fff6ff;
+        }
+
+        .bnts button:last-child{
+            position: absolute;
+            right: 0;
+            background-color: #ffffff;
+            color: #1d165cff;
+        }
+
+        .bnts button:first-child:hover{
+            color: #1d165cff;
+        }
+        .bnts button:hover{
+            background-color: #ffcd94ff;
+        }
+
+        .bnts i{
+            margin-right: 2px;
+            font-size: 15px;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="box_add">
+        <h1>Adicionar um Novo Livro</h1>
+
+        <form action="" method="POST">
+            @csrf
+            <div class="campo_titulo">
+                <label for="titulo">Titulo: </label>
+                <input type="text" placeholder="Digite o nome do livro" name="titulo" id="titulo">
+            </div>
+            <div class="campo_autor">
+                <label for="nome_autor">Nome do autor: </label>
+                <input type="text" placeholder="Digite o nome do autor" name="nome_autor" id="nome_autor" >
+            </div>
+            <div class="campo_preco">
+                <label for="preco">Preço: </label>
+                <input type="text" placeholder="Digite o preço do livro" name="preco" id="preco" >
+            </div>
+            <div class="campo_categoria">
+                <label for="categoria">Categoria: </label>
+                <input type="text" placeholder="Digite a categoría" name="categoria" id="categoria">
+            </div>
+            <div class="campo_ano">
+                <label for="ano">Ano do livro: </label>
+                <input type="text" placeholder="Digite o ano do livro" name="ano" id="ano">
+            </div>
+            
+            
+        </form>
+        
+        <div class="bnts">
+             <button><i class='bx  bx-chevrons-left'></i>Voltar</button>
+            <button><i class='bxr  bx-plus'  ></i>  Adicionar</button>
+        </div>
+    </div>
+    
+</body>
+</html>
--- resources/views/livros/index.blade.php
+++ resources/views/livros/index.blade.php
@@ -4,16 +4,126 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+        body{
+            width: 100%;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            align-items: center;
+        }
+        *{
+            font-family: "Poppins", sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        .box_livros{
+            width: 80%;
+            height: 450px;
+            background-color: #eeeeeeff;
+            border-radius: 15px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            position: absolute;
+        }
+        .box_livros h1{
+            position: absolute;
+            top: 10px;
+            font-size: 25px;
+            color: #1d165cff;
+        }
+
+        .card_livro{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 10px;
+            border-radius: 10px;
+            width: 220px;
+            height: 300px;
+            background-color: #ffffff;
+            position: absolute;
+        }
+
+        .card_livro .img{
+            border-radius: 5px;
+            width: 100%;
+            height: 90px;
+            background-color: #1d165cff;
+        }
+
+        .detalhes{
+            width: 100%;
+            height: 150px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+
+        .detalhes p{
+            font-size: 15px;
+            color: #0e0b2cff;
+            
+        }
+
+        .bnts{
+            display: flex;
+            width: 100%;
+            position: relative;
+        }
+
+        .bnts button{
+            cursor: pointer;
+            width: 80px;
+            height: 30px;
+            border: none;
+            border-radius: 5px;
+            outline: none;
+            transition: all .20s linear;
+        }
+
+        .bnts button:hover{
+            background-color: #ffcd94ff;
+        }
+
+        .bnts button:first-child{
+            background-color: #1d165cff;
+            color: #94fff6ff;
+        }
+        .bnts button:first-child:hover{
+            background-color: #ffcd94ff;
+            color: #1d165cff;
+        }
+
+        .bnts button:last-child{
+            position: absolute;
+            right: 0;
+        }
+    </style>
+    
 </head>
 <body>
     <div class="box_livros">
         <h1>Veja os livros desponíveis</h1>
         <div class="card_livro">
-            <div class="img">
-                <img src="image_card.jpeg" alt="" srcset="">
+            <div class="img"></div>
+            <div class="detalhes">
+                <p>Titulo: </p>
+                <p>Nome do autor : </p>
+                <p>Preço : </p>
+                <p>Categoria : </p>
+                <p>Ano de lançamento: </p>
             </div>
-            <div class="detalhes"></div>
-            <div class=""></div>
+            <div class="bnts">
+                <button id="bntEditar">Editar</button>
+                <button id="bntEditar">Excluir</button>
+            </div>
         </div>
     </div>
 </body>
