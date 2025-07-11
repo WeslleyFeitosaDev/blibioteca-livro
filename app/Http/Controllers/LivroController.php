9bcc477aa3a3de8f410da4f9426e80dd88808d12
Weslley Feitosa Barbosa <weslleybarbosa662@gmail.com>
2025-07-11 15:18:13 -0300

    adicionando o que precisa para o projeto

--- app/Http/Controllers/LivroController.php
+++ app/Http/Controllers/LivroController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Livro;
 
 class LivroController extends Controller
 {
@@ -11,7 +12,8 @@ class LivroController extends Controller
      */
     public function index()
     {
-        //
+        $livros = Livro::all();
+        return view("");
     }
 
     /**
